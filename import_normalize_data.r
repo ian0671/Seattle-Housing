175afe0272728c66c4caa3f868b71162983af117
ian0671 <ian.knightly@outlook.com>
2025-04-13 19:02:19 -0400

    Computed Bootstrap Simulation

--- import_normalize_data.r
+++ import_normalize_data.r
@@ -1,3 +1,7 @@
+# Load necessary libraries
+library(dplyr)
+library(readr)
+
 # Set the destination directory
 destination <- file.path(getwd(), "housing-price-dataset.zip")
 
@@ -21,19 +25,30 @@ cat("File successfully unzipped to:", unzip_dir, "\n")
 
 data <- read_csv("housing-price-dataset/Housing.csv")
 
-# Load necessary libraries
-library(dplyr)
+# Calculate quantiles
+quantiles <- quantile(data$sqft_living, probs = c(0.25, 0.5, 0.75), na.rm = TRUE)
+
+# Create a new column 'blocks' based on the quantiles
 
 data<-data%>%
 mutate(bathrooms = ifelse(is.na(as.numeric(bathrooms)), 0, as.numeric(bathrooms)), 
 zipcode = as.factor(zipcode), 
-bed_bth_ratio = ifelse(bedrooms == 0, 0, bathrooms / bedrooms), 
-`zip code region` = as.factor(ifelse(grepl("980", zipcode), "Region 1", ifelse(grepl("981", zipcode), "Region 2", "Region 3"))), 
-`factor levels` = ifelse(bed_bth_ratio == 1, "factor 1", ifelse(bed_bth_ratio > 1, "factor 2", "factor 3")), 
+bed_bth_ratio = ifelse(bathrooms == 0, 0, bathrooms/bedrooms), 
+`zip code region` = as.factor(ifelse(grepl("980", zipcode), "Region 1", ifelse(grepl("981", zipcode), "Region 2", "Region 3"))),
+`zip code region` = as.factor(`zip code region`), 
+`factor levels` = ifelse(bed_bth_ratio < 1, "Factor Level 1", "Factor Level 2"),
+`factor levels` = as.factor(`factor levels`),
 price = log(price), 
-sqft_living = log(sqft_living), sqft_living15 = log(sqft_living15),
-blocks = ifelse(sqft_living < 7.258, "Block 1", ifelse(sqft_living >= 7.258 & sqft_living < 7.550, "Block 2", ifelse(sqft_living >= 7.550 & sqft_living < 7.832, "Block 3", "Block 4"))))%>%
-filter_all(all_vars(!is.na(.)))%>%select(-yr_renovated)
+blocks = case_when(
+      sqft_living < quantiles[1] ~ "Block 1",
+      sqft_living >= quantiles[1] & sqft_living < quantiles[2] ~ "Block 2",
+      sqft_living >= quantiles[2] & sqft_living < quantiles[3] ~ "Block 3",
+      TRUE ~ "Block 4"
+    ),
+blocks = as.factor(blocks),
+sqft_living = log(sqft_living), sqft_living15 = log(sqft_living15))%>%
+filter_all(all_vars(!is.na(.)))%>%
+select(-yr_renovated)
 
 # Remove outliers in the dataset
 remove_outliers <- function(data, column) {
@@ -66,6 +81,10 @@ data<-remove_outliers(data, "sqft_living")
 data<-remove_outliers(data, "sqft_living15")
 
 write_csv(data, "Preprocessed_Housing.csv")
+
+
+print(destination)
+print(unzip_dir)
 
 # Clean up: remove the downloaded zip file and unzipped directory
 file.remove(destination)
